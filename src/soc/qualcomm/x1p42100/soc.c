d8d6bac6c03b61e0f0f9c7da045ce5e1d7348f79
Kapil Porwal <kapilporwal@google.com>
2026-09-04 18:21:45 +0530

    soc/qualcomm/x1p42100: Remove unused dram_wlan region
    BUG=none
    TEST=Verify connectivity on Google/Mica.
    
    Change-Id: Ibd521d776f67aa490b0c6c2c86a79b10d9cc91c0
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/95236
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/soc/qualcomm/x1p42100/soc.c
+++ src/soc/qualcomm/x1p42100/soc.c
@@ -102,7 +102,6 @@ static void soc_read_resources(struct device *dev)
 	reserved_ram_range(dev, index++, (uintptr_t)_dram_pld_pep, REGION_SIZE(dram_pld_pep));
 	reserved_ram_range(dev, index++, (uintptr_t)_dram_pld_gmu, REGION_SIZE(dram_pld_gmu));
 	reserved_ram_range(dev, index++, (uintptr_t)_dram_pld_pdp, REGION_SIZE(dram_pld_pdp));
-	reserved_ram_range(dev, index++, (uintptr_t)_dram_wlan, REGION_SIZE(dram_wlan));
 	reserved_ram_range(dev, index++, (uintptr_t)_dram_pil, REGION_SIZE(dram_pil));
 	reserved_ram_range(dev, index++, (uintptr_t)_dram_ta, REGION_SIZE(dram_ta));
 
